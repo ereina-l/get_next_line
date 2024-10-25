92c8eb488212b1912771401705fa1edc87d0955f
Elena Reina Lopez <ereina-l@c1r9s5.42malaga.com>
2024-10-25 16:50:45 +0200

    updates

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,32 @@
 /*   By: ereina-l <ereina-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 15:30:03 by ereina-l          #+#    #+#             */
-/*   Updated: 2024/10/25 16:22:05 by ereina-l         ###   ########.fr       */
+/*   Updated: 2024/10/25 16:46:26 by ereina-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
-char	*get_next_line(int fd)
+char	*ft_get_extract(int fd)
 {
 	void	*tmp;
 	int		rvalue;
 
-	tmp = (void *)malloc(BUFFER_SIZE * sizeof(char));
-	if (!tmp)
-		return (NULL);
-	rvalue = read(fd, tmp, BUFFER_SIZE);
-	if (rvalue <= 0)
-		return (NULL);
+	while (1)
+	{
+		tmp = (void *)malloc(BUFFER_SIZE * sizeof(char));
+		if (!tmp)
+			return (NULL);
+		rvalue = read(fd, tmp, BUFFER_SIZE);
+		if (rvalue <= 0)
+			return (NULL);
+		if (ft_find_newline((char *)tmp))
+			return (tmp);
+	}
+}
+char	*get_next_line(int fd)
+{
+	char *tmp = ft_get_extract(fd);
 	return (tmp);
 }
 
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,28 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ereina-l <ereina-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/25 15:30:33 by ereina-l          #+#    #+#             */
-/*   Updated: 2024/10/25 16:34:44 by ereina-l         ###   ########.fr       */
+/*   Created: 2024/10/25 16:31:50 by ereina-l          #+#    #+#             */
+/*   Updated: 2024/10/25 16:34:02 by ereina-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include "get_next_line.h"
 
-# include <stdio.h>
-# include <fcntl.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 50
-# endif
-
-char	*get_next_line(int fd);
-char	*ft_find_newline(char *s);
-
-#endif
+char	*ft_find_newline(char *s)
+{
+	while (*s != '\0')
+	{
+		if (*s == 10)
+			return (s);
+		s++;
+	}
+	return (0);
+}
